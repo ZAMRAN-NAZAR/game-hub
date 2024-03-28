52919002cba6d54b0658e91f40007a683ccf154c
Zamran Nazar <zamrannazar1@gmail.com>
2024-03-28 21:36:15 +0530

    Fetch the genres

---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+interface Genre {
+  id: number;
+  name: string;
+}
+
+interface fetchGenresResponse {
+  count: number;
+  results: Genre[];
+}
+
+const useGenres = () => {
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    apiClient
+      .get<fetchGenresResponse>("/genres")
+      .then((res) => {
+        setGenres(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false)
+      });
+
+  }, []);
+
+  return { genres, error, isLoading};
+}
+
+export default useGenres;
